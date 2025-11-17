88d22d2072b337a1d3ec10eaf214aa94b245e93f
velavan rahul <ananymous980@gmail.com>
2025-11-17 08:50:46 +0530

    intialize frontend

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,5 @@ app.use('/api/auth', authRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
